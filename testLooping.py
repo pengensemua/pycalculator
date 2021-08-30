9ecfccf6ea3eda9037931a9f1aa152f2c8291ed8
pengensemua <super.pengen@gmail.com>
2021-08-30 12:21:03 +0700

    Testing Infinite Loop by using while

---
+++ testLooping.py
@@ -0,0 +1,12 @@
+# count = 0
+# while (count<9):
+#     print('Hitung :', count)
+#     count = count + 1
+#
+# print("Dadah")
+var = 1
+while var == 1:  #ini akan menjadikan looping tanpa akhir sampai ada error atau stop manual
+    num = int(input("Masukkan Angka: "))
+    print ("Kamu memasukkan Angka: ", num)
+
+print ("Dadah")
